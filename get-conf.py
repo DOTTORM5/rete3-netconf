5d7c9d0dd2500f2b9a54fe77414e664a12af6a30
DOTTORM5 <manuelmaddaluno5@gmail.com>
2021-05-03 09:47:53 +0200

    Update get-conf.py

--- get-conf.py
+++ get-conf.py
@@ -4,8 +4,14 @@ import my_xml
 
 m = manager.connect(host="10.10.13.2", port=830, username="cisco", password="cisco", hostkey_verify=False)
 
-
-reply = m.edit_config(target="running", config=my_xml.ospf)
-# reply = m.get_config(source="running")
+#configurazione generale: hostname, nuovo username, password...
+reply = m.edit_config(target="running", config=my_xml.config_global) 
 print(xml.dom.minidom.parseString(reply.xml).toprettyxml())
 
+#configurazione interfacce
+reply = m.edit_config(target="running", config=my_xml.interface_confg)
+print(xml.dom.minidom.parseString(reply.xml).toprettyxml())
+
+#configurazione OSPF
+reply = m.edit_config(target="running", config=my_xml.ospf)
+print(xml.dom.minidom.parseString(reply.xml).toprettyxml())
